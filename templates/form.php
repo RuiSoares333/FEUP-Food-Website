2f6a11e2084ac495c2e900545294408d41af4fc8
Rui Soares <up202103631@up.pt>
2022-05-07 01:00:07 +0100

    CSS do Login
    CSS mostly done

--- templates/form.php
+++ templates/form.php
@@ -1,18 +1,16 @@
 <?php
     function ouputLoginForm() { ?>
-        <section id = "login">
-            <h1>Login</h1>
-            <form>
-                <label>
-                    Username <input type="text" name="username">
-                </label>
-                <label>
-                    Password <input type="password" name="password">
-                </label>
-                <input type="hidden" name = "referer" value="<?=$_SERVER['HTTP_REFERER']?>">
-                <button formaction="../actions/action_login.php" formmethod="post">Login</button>
-            </form>
-            <a class = "register" href="register.php"><h5>Not Registered?</h5></a>
-        </section>
+    <div class="mainDiv" id="login">
+        <br>
+        <p id="square"></p>
+        <h1>Login</h1>
+        <form id="loginForm">
+            <input type="text" name="username" placeholder="username">
+            <input type="password" name="password" placeholder="password">
+            <input type="hidden" name = "referer" value="<?=$_SERVER['HTTP_REFERER']?>">
+            <button formaction="../actions/action_login.php" formmethod="post">Login</button>
+        </form>
+        <a class = "register" href="register.php"><h5>Not Registered?</h5></a>
+    </div>
     <?php }
 ?>
