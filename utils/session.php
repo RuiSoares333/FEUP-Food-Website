44ec024acafb5318e98c76386414140567702918
Gonçalo Marques <goncalomarques.abacate@gmail.com>
2022-06-14 03:50:49 +0100

    now im done with xss

--- utils/session.php
+++ utils/session.php
@@ -3,6 +3,7 @@
         private array $messages;
 
         public function __construct(){
+            session_set_cookie_params(0, '/', 'www.fe.up.pt', true, true);
             session_start();
             $this->messages = isset($_SESSION['messages']) ? $_SESSION['messages'] : array();
             unset($_SESSION['messages']);
